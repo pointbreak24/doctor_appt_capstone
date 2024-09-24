556b51b734cbd140cdd868b8e4cfac1c83e0ac8f
pointbreak24 <pointbreak2471@gmail.com>
2024-09-24 11:50:04 -0400

    Added mongoDB info

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:uo9Ce8gGcZwdM6M8oPGQSOIR@172.21.39.214:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
